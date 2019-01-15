7bae9186f27e254110002dd6bbaa8781266b60eb
k2controls <kkelly@k2controls.net>
2019-01-15 07:24:30 -0500

    Initial commit

---
+++ iot_gh_ws_intro.py
@@ -0,0 +1,18 @@
+from time import sleep
+from iot_gh.IoTGreenhouseService import IoTGreenhouseService
+from iot_gh.GHTextingService import GHTextingService
+
+TOKEN = "your token here"
+last_id = None
+
+ghs = IoTGreenhouseService()
+ts = GHTextingService(ACCESS_TOKEN, ghs)
+
+while True:
+    lm = ts.last_message
+    if lm.id != last_id:
+        print(lm.name + "   " + lm.text)
+        print()
+
+        last_id = lm.id
+    sleep(.5)
